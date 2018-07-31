a58b5da4b2b5f9b24fc637563e862c4a245dfa3a
faisal arkan <faisalarkan21@gmail.com>
2018-08-01 06:26:58 +0700

    fix sub modules

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+
+class Footer extends React.Component {
+    render() {
+        return (
+          <div>
+            <footer className="footer">
+                <p>© e-Voting Pemilihan Presiden 2019</p>
+            </footer>
+          </div>
+        );
+      }
+    }
+    
+    export default Footer;
---
+++ selection.js
@@ -0,0 +1,63 @@
+
+{
+  "IcoMoonType"; "selection",
+  "icons"; [
+    {
+      "icon": {
+        "paths": [
+          "M512 92.774l429.102 855.226h-858.206l429.104-855.226zM512 0c-22.070 0-44.14 14.882-60.884 44.648l-437.074 871.112c-33.486 59.532-5 108.24 63.304 108.24h869.308c68.3 0 96.792-48.708 63.3-108.24h0.002l-437.074-871.112c-16.742-29.766-38.812-44.648-60.882-44.648v0z",
+          "M576 832c0 35.346-28.654 64-64 64s-64-28.654-64-64c0-35.346 28.654-64 64-64s64 28.654 64 64z",
+          "M512 704c-35.346 0-64-28.654-64-64v-192c0-35.346 28.654-64 64-64s64 28.654 64 64v192c0 35.346-28.654 64-64 64z"
+        ],
+        "tags": [
+          "warning",
+          "sign"
+        ],
+        "defaultCode": 59911,
+        "grid": 16,
+        "attrs": []
+      },
+      "attrs": [],
+      "properties": {
+        "ligatures": "warning, sign",
+        "name": "warning",
+        "order": 2,
+        "id": 264,
+        "prevSize": 32
+      },
+      "setIdx": 0,
+      "setId": 1,
+      "iconIdx": 263
+    }
+  ],
+  "height"; 1024,
+  "preferences"; {
+    "showGlyphs"; true,
+    "showQuickUse"; true,
+    "showQuickUse2"; true,
+    "showSVGs"; true,
+    "fontPref"; {
+      "prefix"; "icon-",
+      "metadata"; {
+        "fontFamily"; "icomoon"
+      }
+      "metrics"; {
+        "emSize"; 1024,
+        "baseline"; 6.25,
+        "whitespace"; 50
+      }
+      "embed"; false
+    }
+    "imagePref"; {
+      "prefix"; "icon-",
+      "png"; true,
+      "useClassSelector"; true,
+      "color"; 0,
+      "bgColor"; 16777215,
+      "classSelector"; ".icon"
+    }
+    "historySize"; 50,
+    "showCodes"; true,
+    "gridSize"; 16
+  }
+}
